4dd00f20f8c14c5d40438a70d387e82f3f78b7a8
Sumeet Rohatgi <sum.rohatgi@gmail.com>
2017-02-03 14:37:09 -0800

    started logging

--- src/main/java/com/intocloudtech/paas/mq/MQSpec.java
+++ src/main/java/com/intocloudtech/paas/mq/MQSpec.java
@@ -1,21 +1,31 @@
 package com.intocloudtech.paas.mq;
 
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.ecs.AmazonECS;
 import com.amazonaws.services.ecs.AmazonECSClientBuilder;
+import com.amazonaws.services.ecs.model.Cluster;
 import com.amazonaws.services.ecs.model.CreateClusterRequest;
 import com.amazonaws.services.ecs.model.CreateClusterResult;
 
+import com.amazonaws.services.ecs.model.DescribeClustersRequest;
 import com.beust.jcommander.JCommander;
 
 import com.beust.jcommander.Parameter;
+import com.sun.tools.javac.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
 
 
 /**
  * build a spec for mq-service cluster
- * for launching in aws-ecs
+ * for launching with different recipes
  */
 public class MQSpec {
+    static final Logger logger = LoggerFactory.getLogger(MQSpec.class);
+
     @Parameter(names = { "--instances" }, description = "Instances of mq containers")
     int instances = 1;
     @Parameter(names = { "--storage" }, description = "Storage in GiB for each mq container")
@@ -23,27 +33,57 @@ public class MQSpec {
     @Parameter(names = { "--cluster" }, description = "Cluster name")
     String clusterName = "j-sumeet";
 
+    AmazonECS ecs;
+
     public static void main(String... args) {
         MQSpec spec = new MQSpec();
         new JCommander(spec, args);
-        spec.run();
-    }
 
-    void run() {
-        AmazonECS ecs = AmazonECSClientBuilder
+        spec.ecs = AmazonECSClientBuilder
                 .standard()
+                .withCredentials(new ProfileCredentialsProvider())
                 .withRegion(Regions.US_WEST_1).build();
 
-        long clusterCount = ecs.describeClusters()
+        spec.runRecipe1();
+    }
+
+    void runRecipe1() {
+        Cluster cluster = buildCluster();
+
+        List<String> zkIps = bootZooKeeper(cluster);
+
+        bootKafka(cluster, zkIps);
+    }
+
+    private void bootKafka(Cluster cluster, List<String> zkIps) {
+
+    }
+
+    Cluster buildCluster() {
+        logger.debug("finding cluster: {}", clusterName);
+
+        DescribeClustersRequest describe = new DescribeClustersRequest().withClusters(clusterName);
+
+        Optional<Cluster> cluster = ecs.describeClusters(describe)
                 .getClusters()
                 .stream()
-                .filter(c -> c.getClusterName().equals(clusterName))
-                .count();
+                .findAny();
 
-        if (clusterCount == 0) {
+        if (!cluster.isPresent()) {
+            logger.info("creating cluster {}", clusterName);
+
             CreateClusterRequest request = new CreateClusterRequest().withClusterName(clusterName);
 
             CreateClusterResult result = ecs.createCluster(request);
+            cluster = Optional.of(result.getCluster());
         }
+
+        logger.debug("cluster exists: {}", cluster.get().getClusterName());
+
+        return cluster.get();
+    }
+
+    private List<String> bootZooKeeper(Cluster cluster) {
+        return null;
     }
 }
